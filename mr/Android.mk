5606465767679282780124a9f613c197a04c921f
Ido Reis <idor@ti.com>
2012-01-08 00:17:48 +0200

    added mr Android.mk and licence file
    Signed-off-by: Ido Reis <idor@ti.com>

---
+++ mr/Android.mk
@@ -0,0 +1,122 @@
+LOCAL_PATH := $(call my-dir)
+local_target_dir := $(TARGET_OUT_DATA)/misc/wifi
+
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := ap_start.sh
+#LOCAL_MODULE_TAGS := debug
+#LOCAL_MODULE_CLASS := SCRIPT
+#LOCAL_MODULE_PATH := $(local_target_dir)
+#LOCAL_SRC_FILES := $(LOCAL_MODULE)
+#include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := aGO_add.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := aGO_remove.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mr_ago_sta.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mr_ap_sta.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mr_sta_ago.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mr_sta_ap.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sta_add.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sta_remove.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := p2p_add.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := p2p_remove.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ap_add.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ap_remove.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := FreqToCh.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ChToFreq.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
