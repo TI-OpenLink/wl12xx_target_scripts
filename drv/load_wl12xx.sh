cf263695ddc136f409a45018c684a82d43bec144
Ido Reis <idor@ti.com>
2012-02-27 17:07:37 +0200

    add wl12xx load script

---
+++ drv/load_wl12xx.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+
+insmod /system/lib/modules/compat.ko
+insmod /system/lib/modules/cfg80211.ko
+insmod /system/lib/modules/mac80211.ko
+insmod /system/lib/modules/wl12xx.ko 
+#debug_level=0x63c00
+
