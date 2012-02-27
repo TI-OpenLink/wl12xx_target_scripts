383be479d1db1953eabd250281fa405f6608abeb
Ido Reis <idor@ti.com>
2012-02-27 17:07:37 +0200

    add wl12xx load script

--- drv/Android.mk
+++ drv/Android.mk
@@ -2,6 +2,14 @@ LOCAL_PATH := $(call my-dir)
 local_target_dir := $(TARGET_OUT_DATA)/misc/wifi
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := load_wl12xx.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := load_wlcore_com8_siso40.sh
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_CLASS := SCRIPT
