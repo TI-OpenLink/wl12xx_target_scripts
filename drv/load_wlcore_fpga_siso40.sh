c89de23f4d811e42b1d70da9b37a8f4bf13ae21f
Ido Reis <idor@ti.com>
2012-03-26 16:30:59 +0200

    wlcore is using ibi as default

--- drv/load_wlcore_fpga_siso40.sh
+++ drv/load_wlcore_fpga_siso40.sh
@@ -3,7 +3,7 @@
 insmod /system/lib/modules/compat.ko
 insmod /system/lib/modules/cfg80211.ko
 insmod /system/lib/modules/mac80211.ko
-insmod /system/lib/modules/wlcore.ko
+insmod /system/lib/modules/wlcore.ko irq=sdio
 insmod /system/lib/modules/wl12xx.ko
 insmod /system/lib/modules/wl18xx.ko board_type=fpga ht_mode=wide n_antennas_2=1 n_antennas_5=1 dc2dc=0
 
