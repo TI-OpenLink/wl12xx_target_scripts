0b98d53bdbc333a5e34453e087849a79ab549d5b
Ido Reis <idor@ti.com>
2012-03-26 16:30:59 +0200

    wlcore is using ibi as default

--- drv/load_wlcore_hdk_rld1_rdl5_siso40.sh
+++ drv/load_wlcore_hdk_rld1_rdl5_siso40.sh
@@ -3,6 +3,6 @@
 insmod /system/lib/modules/compat.ko
 insmod /system/lib/modules/cfg80211.ko
 insmod /system/lib/modules/mac80211.ko
-insmod /system/lib/modules/wlcore.ko
+insmod /system/lib/modules/wlcore.ko irq=sdio
 insmod /system/lib/modules/wl12xx.ko
 insmod /system/lib/modules/wl18xx.ko board_type=hdk ht_mode=wide n_antennas_2=1 n_antennas_5=1 dc2dc=1
--- drv/load_wlcore_hdk_rld2_rdl7_mimo.sh
+++ drv/load_wlcore_hdk_rld2_rdl7_mimo.sh
@@ -3,7 +3,7 @@
 insmod /system/lib/modules/compat.ko
 insmod /system/lib/modules/cfg80211.ko
 insmod /system/lib/modules/mac80211.ko
-insmod /system/lib/modules/wlcore.ko
+insmod /system/lib/modules/wlcore.ko irq=sdio
 insmod /system/lib/modules/wl12xx.ko
 insmod /system/lib/modules/wl18xx.ko board_type=hdk ht_mode=mimo n_antennas_2=2 n_antennas_5=2 dc2dc=0
 
