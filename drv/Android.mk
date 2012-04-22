1d6a6ca22b30cc02b78911cc2fe13745f0d323fd
Ido Reis <idor@ti.com>
2012-04-22 11:55:38 +0300

    pg2 scripts added (hdk rdl2/7 configuration)

--- drv/Android.mk
+++ drv/Android.mk
@@ -2,6 +2,30 @@ LOCAL_PATH := $(call my-dir)
 local_target_dir := $(TARGET_OUT_DATA)/misc/wifi
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := load_wlcore_pg2_siso20.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := load_wlcore_pg2_siso40.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := load_wlcore_pg2_mimo.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := load_wl12xx.sh
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_CLASS := SCRIPT
