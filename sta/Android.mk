8ab06880b0a6ebfee497c22fa0b44a7a068f8322
Ido Reis <idor@ti.com>
2012-05-07 15:07:09 +0300

    sta: added connect script
    Signed-off-by: Ido Reis <idor@ti.com>

--- sta/Android.mk
+++ sta/Android.mk
@@ -25,3 +25,11 @@ LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := sta_connect.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
