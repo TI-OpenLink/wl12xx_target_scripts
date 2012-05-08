fff91b8c3dae2c5419a74fed9b565e37efef7fa8
Ido Reis <idor@ti.com>
2012-05-08 14:27:14 +0300

    drv: added unload_wlcore.sh
    Signed-off-by: Ido Reis <idor@ti.com>

--- drv/Android.mk
+++ drv/Android.mk
@@ -177,3 +177,11 @@ LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := unload_wlcore.sh
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_CLASS := SCRIPT
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
