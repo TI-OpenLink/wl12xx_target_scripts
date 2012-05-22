166e9a0657ba9562feab20fa47cd8f66a86a73a8
Ido Reis <idor@ti.com>
2012-05-22 13:36:56 +0300

    r5.1 support only wl12xx

--- drv/load_wlcore.sh
+++ drv/load_wlcore.sh
@@ -3,6 +3,7 @@
 insmod /system/lib/modules/compat.ko
 insmod /system/lib/modules/cfg80211.ko
 insmod /system/lib/modules/mac80211.ko
-insmod /system/lib/modules/wl12xx.ko 
-#debug_level=0x63c00
+#insmod /system/lib/modules/wlcore.ko irq=sdio
+insmod /system/lib/modules/wlcore.ko
+insmod /system/lib/modules/wl12xx.ko
 
