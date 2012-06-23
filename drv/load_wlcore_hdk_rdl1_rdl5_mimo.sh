b886bebc1d21c2c83a8950d94bfc80801925141c
Ido Reis <idor@ti.com>
2012-06-24 01:26:03 +0300

    disable IBI as default
    Signed-off-by: Ido Reis <idor@ti.com>

--- drv/load_wlcore_hdk_rdl1_rdl5_mimo.sh
+++ drv/load_wlcore_hdk_rdl1_rdl5_mimo.sh
@@ -3,6 +3,6 @@
 insmod /system/lib/modules/compat.ko
 insmod /system/lib/modules/cfg80211.ko
 insmod /system/lib/modules/mac80211.ko
-insmod /system/lib/modules/wlcore.ko irq=sdio
+insmod /system/lib/modules/wlcore.ko
 insmod /system/lib/modules/wl12xx.ko
 insmod /system/lib/modules/wl18xx.ko low_band_component=2 low_band_component_type=4 high_band_component_type=9 board_type=hdk ht_mode=mimo n_antennas_2=2 n_antennas_5=2 dc2dc=1
