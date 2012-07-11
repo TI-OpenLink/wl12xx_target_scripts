5849a23f493c37e2eab8487736bb7bf0d2a47849
Ido Reis <idor@ti.com>
2012-07-11 17:51:19 +0300

    drv: remove wl12xx load script

--- drv/load_wl12xx.sh
+++
@@ -1,8 +0,0 @@
-#!/system/bin/sh
-
-insmod /system/lib/modules/compat.ko
-insmod /system/lib/modules/cfg80211.ko
-insmod /system/lib/modules/mac80211.ko
-insmod /system/lib/modules/wl12xx.ko 
-#debug_level=0x63c00
-
