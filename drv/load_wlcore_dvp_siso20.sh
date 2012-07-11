7c1256d9aee881b217e3bb1779be4d18b267db92
Ido Reis <idor@ti.com>
2012-07-11 17:51:52 +0300

    drv: remove wl18xx pg1 board support

--- drv/load_wlcore_dvp_siso20.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-
-insmod /system/lib/modules/compat.ko
-insmod /system/lib/modules/cfg80211.ko
-insmod /system/lib/modules/mac80211.ko
-insmod /system/lib/modules/wlcore.ko irq=sdio
-insmod /system/lib/modules/wl12xx.ko
-insmod /system/lib/modules/wl18xx.ko low_band_component=2 low_band_component_type=4 high_band_component_type=9 board_type=dvp ht_mode=siso20 n_antennas_2=1 n_antennas_5=1 dc2dc=0
-
