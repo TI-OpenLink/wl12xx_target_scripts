363af834ff3e78d4902111525c9f603af337c905
Ido Reis <idor@ti.com>
2012-07-12 20:28:16 +0300

    testing: update set wlcore script to use wlconf

--- root/testing_set_wlcore.sh
+++ root/testing_set_wlcore.sh
@@ -1,35 +1,74 @@
 #!/data/busybox/sh
 #
-# $1 = mimo/siso
-# $2 = hw type - com8/rdl7/etc... (optional)
+# $1 = mimo/siso20/siso40
+# $2 = hw type - rdl1_rdl3/rdl2_rdl4
+
+echo $@
+
+# Remount system partition as rw
+mount -o remount rw /system
 
 root_path=/data/misc/wifi
-load_wlcore_link_fname=${root_path}/load_wlcore.sh
 hw_type_fname=${root_path}/testing_wl_hw_type.txt
+
+wlconf_path=/system/etc/wifi/wlconf
+ini_files_path=${wlconf_path}/official_inis
+
+wl18xx_conf_bin=/system/etc/firmware/ti-connectivity/wl18xx-conf.bin
 
 if [ "$2" != "" ]; then
     echo $2 > $hw_type_fname
 fi
 
 hw_type=`cat $hw_type_fname 2> /dev/null`
-load_wlcore_fname=${root_path}/load_wlcore_${hw_type}_${1}.sh
- 
-if [ ! -f "$load_wlcore_fname" ]; then
-    echo "wlcore error: no such configuration (hw: $hw_type, $1)"
+
+ht_mimo=0
+ht_siso20=2
+ht_siso40=1
+
+#
+# verify ht_mode, options are: siso20, siso40, mimo
+#
+if [ "$1" == "siso20" ] ; then
+    ht_mode=${ht_siso20}
+elif  [ "$1" == "siso40" ] ; then
+    ht_mode=${ht_siso40}
+elif  [ "$1" == "mimo" ] ; then
+    ht_mode=${ht_mimo}
+else
+    echo "wlcore: not supported"
     exit 1
 fi
 
-if [ -f $load_wlcore_link_fname ] ; then
-    rm -f $load_wlcore_link_fname
-fi
-
-ln -sf $load_wlcore_fname $load_wlcore_link_fname
-
-if [ $? != 0 ]; then
-    echo "wlcore error: link failure"
+#
+# select chip type, options are: rdl2_rdl4 (mimo board), rdl1_rdl2 (hp siso board)
+#
+if [ "$hw_type" == "rdl2_rdl4" ] ; then # mimo board
+    if [ "$ht_mode" == "$ht_mimo" ] ; then # mimo
+	ini_file=WL8_System_parameters_PG2_RDL_2_4_SP_MIMO.ini
+    elif [ "$ht_mode" == "$ht_siso40" ] || [ "$ht_mode" == "$ht_siso20" ] ; then # siso
+	ini_file=WL8_System_parameters_PG2_RDL_2_4_SP_SISO.ini
+    else
+	echo "wlcore: not supported"
+	exit 1
+    fi
+elif [ "$hw_type" == "rdl1_rdl3" ] ; then # hp siso board
+    if [ "$ht_mode" == "$ht_siso40" ] || [ "$ht_mode" == "$ht_siso20" ] ; then # siso
+        ini_file=WL8_System_parameters_PG2_RDL_1_3_HP_SISO.ini
+    else
+	echo "wlcore: not supported"
+	exit 1
+    fi
+else
+    echo "wlcore: not supported"
     exit 1
 fi
 
-echo "wlcore: configuration ok ($load_wlcore_fname)"
+cd ${wlconf_path}
+wlconf -o ${wl18xx_conf_bin} -I ${ini_files_path}/${ini_file}
+wlconf -i ${wl18xx_conf_bin} -o ${wl18xx_conf_bin} --set wl18xx.ht.mode=${ht_mode}
+wlconf -i ${wl18xx_conf_bin} -g | grep -i "board\|ant\|ht.mode"
+
+echo "wlcore: configuration ok"
 exit 0
 
