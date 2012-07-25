0cf688e7339c44b60ec06139de251c5e6f4ccded
Ido Reis <idor@ti.com>
2012-07-25 18:06:09 +0300

    mr: remove p2p_supplicant.conf

--- mr/Android.mk
+++ mr/Android.mk
@@ -2,14 +2,6 @@ LOCAL_PATH := $(call my-dir)
 local_target_dir := $(TARGET_OUT_DATA)/misc/wifi
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := p2p_supplicant.conf
-LOCAL_MODULE_TAGS := debug
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := aGO_add.sh
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_CLASS := SCRIPT
