24d1ee3c5cb5eed0aa1f5f64ab5bfc3e67555769
Ido Reis <idor@ti.com>
2012-08-09 13:09:52 +0300

    mr: update sta add/rem scripts to jb

--- mr/sta_remove.sh
+++ mr/sta_remove.sh
@@ -8,18 +8,21 @@ WPA_CLI=/system/bin/wpa_cli
 IW=/system/bin/iw
 
 WLAN_IF=wlan0
+P2P_WLAN_IF=p2p0
 
-SERVICE_SUPPLICANT=wpa_supplicant
+SERVICE_SUPPLICANT=p2p_supplicant
 
 echo "unload supplicant"
 setprop ctl.stop $SERVICE_SUPPLICANT
 sleep 1
 
-echo "disable interface"
+echo "disable interfaces"
+$IFCONFIG $P2P_WLAN_IF down
 $IFCONFIG $WLAN_IF down
 sleep 1
 
-echo "Removing interface"
+echo "Removing interfaces"
+$IW $P2P_WLAN_IF del
 $IW $WLAN_IF del
 sleep 1
 
