143f7ac080a969124f94138166cde7436860ef9c
Ido Reis <idor@ti.com>
2012-08-09 13:08:02 +0300

    sta: update to jb

--- sta/sta_start.sh
+++ sta/sta_start.sh
@@ -1,16 +1,23 @@
 #!/system/bin/sh
 
+IW=/system/bin/iw
 INSMOD=/system/bin/insmod
 IFCONFIG=/system/bin/ifconfig
 WPA_SUPPLICANT=/system/bin/wpa_supplicant
 
+
 SUPPLICANT_CONF=/data/misc/wifi/wpa_supplicant.conf
+P2P_SUPPLICANT_CONF=/data/misc/wifi/p2p_supplicant.conf
 
 WLAN_IF=wlan0
 WLAN_IP=192.168.1.20
 WLAN_NETMASK=255.255.255.0
 
-SERVICE_SUPPLICANT=wpa_supplicant
+P2P_WLAN_IF=p2p0
+P2P_WLAN_IP=0.0.0.0
+P2P_WLAN_NETMASK=255.255.255.0
+
+SERVICE_SUPPLICANT=p2p_supplicant
 SERVICE_HOSTAPD=hostapd_bin
 
 SUPP_STAT=`getprop init.svc.$SERVICE_SUPPLICANT`
@@ -25,18 +32,34 @@ echo "loading driver"
 $INSMOD /system/lib/modules/wlcore_sdio.ko
 sleep 1
 
-echo "enable interface"
+PHY=`ls /sys/class/ieee80211/`
+
+echo "adding p2p0 interface"
+$IW $PHY interface add $P2P_WLAN_IF type managed
+sleep 1
+
+echo "enable wlan0 interface"
 $IFCONFIG $WLAN_IF up $WLAN_IP netmask $WLAN_NETMASK
 sleep 1
 $IFCONFIG $WLAN_IF
+
+echo "enable p2p0 interface"
+$IFCONFIG $P2P_WLAN_IF up
+sleep 1
+$IFCONFIG $P2P_WLAN_IF
 
 if [ ! -f $SUPPLICANT_CONF ] ; then \
 	cp /etc/wifi/wpa_supplicant.conf $SUPPLICANT_CONF ; \
 fi
 chmod 777 $SUPPLICANT_CONF
+echo "reseting p2p_supplicant conf"
+cp /etc/wifi/wpa_supplicant.conf $P2P_SUPPLICANT_CONF
+sed -rie "s/ctrl_interface=wlan0/ctrl_interface=\/data\/misc\/wifi\/sockets/" $P2P_SUPPLICANT_CONF
+chmod 777 $P2P_SUPPLICANT_CONF
 
 echo "loading supplicant"
-setprop ctl.start "$SERVICE_SUPPLICANT:-i$WLAN_IF -c$SUPPLICANT_CONF"
-#$WPA_SUPPLICANT -i$WLAN_IF -c$SUPPLICANT_CONF &
+setprop ctl.start "${SERVICE_SUPPLICANT}"
 sleep 1
+
+alias p2p_cli="wpa_cli -i p2p0 -p /data/misc/wifi/sockets/"
 
--- sta/sta_stop.sh
+++ sta/sta_stop.sh
@@ -4,12 +4,20 @@ INSMOD=/system/bin/insmod
 RMMOD=/system/bin/rmmod
 IFCONFIG=/system/bin/ifconfig
 WPA_CLI=/system/bin/wpa_cli
+CHMOD=/system/bin/chmod
 
 WLAN_IF=wlan0
 WLAN_IP=192.168.1.20
 WLAN_NETMASK=255.255.255.0
 
-SERVICE_SUPPLICANT=wpa_supplicant
+P2P_WLAN_IF=p2p0
+P2P_WLAN_IP=0.0.0.0
+P2P_WLAN_NETMASK=255.255.255.0
+
+SERVICE_SUPPLICANT=p2p_supplicant
+
+STA_SUPP_CONF=/data/misc/wifi/wpa_supplicant.conf
+P2P_SUPP_CONF=/data/misc/wifi/p2p_supplicant.conf
 
 SUPP_STAT=`getprop init.svc.$SERVICE_SUPPLICANT`
 echo "wpa_supplicant state: $SUPP_STAT"
@@ -20,10 +28,14 @@ setprop ctl.stop $SERVICE_SUPPLICANT
 #$WPA_CLI -i$WLAN_IF -p/data/misc/wifi/wlan0 terminate
 sleep 1
 
-echo "disable interface"
+echo "disable interfaces"
 $IFCONFIG $WLAN_IF down
+$IFCONFIG $P2P_WLAN_IF down
 sleep 1
 
 echo "unload driver"
 $RMMOD wlcore_sdio
+sleep 1
 
+$CHMOD 777 $STA_SUPP_CONF
+$CHMOD 777 $P2P_SUPP_CONF
