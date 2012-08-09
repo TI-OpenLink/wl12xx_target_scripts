d037951efa0ed0dc3490db502862ae9c454feead
Ido Reis <idor@ti.com>
2012-08-09 13:14:39 +0300

    mr: remove aGo add/rem scripts

--- mr/Android.mk
+++ mr/Android.mk
@@ -2,22 +2,6 @@ LOCAL_PATH := $(call my-dir)
 local_target_dir := $(TARGET_OUT_DATA)/misc/wifi
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := aGO_add.sh
-LOCAL_MODULE_TAGS := debug
-LOCAL_MODULE_CLASS := SCRIPT
-LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := aGO_remove.sh
-LOCAL_MODULE_TAGS := debug
-LOCAL_MODULE_CLASS := SCRIPT
-LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := sta_add.sh
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_CLASS := SCRIPT
