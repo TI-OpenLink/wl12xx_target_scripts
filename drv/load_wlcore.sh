03036326e04d4ad04124387593213398d3d626db
Ido Reis <idor@ti.com>
2012-09-05 14:14:32 +0300

    drv: disable_scan_while_active=0
    Signed-off-by: Ido Reis <idor@ti.com>

--- drv/load_wlcore.sh
+++ drv/load_wlcore.sh
@@ -2,7 +2,7 @@
 
 insmod /system/lib/modules/compat.ko
 insmod /system/lib/modules/cfg80211.ko
-insmod /system/lib/modules/mac80211.ko disable_scan_while_active=1
+insmod /system/lib/modules/mac80211.ko disable_scan_while_active=0
 insmod /system/lib/modules/wlcore.ko
 insmod /system/lib/modules/wl12xx.ko
 insmod /system/lib/modules/wl18xx.ko
--- drv/load_wlcore_rdl2_rdl4_mimo.sh
+++ drv/load_wlcore_rdl2_rdl4_mimo.sh
@@ -2,7 +2,7 @@
 
 insmod /system/lib/modules/compat.ko
 insmod /system/lib/modules/cfg80211.ko
-insmod /system/lib/modules/mac80211.ko disable_scan_while_active=1
+insmod /system/lib/modules/mac80211.ko disable_scan_while_active=0
 insmod /system/lib/modules/wlcore.ko
 insmod /system/lib/modules/wl12xx.ko
 insmod /system/lib/modules/wl18xx.ko board_type=hdk ht_mode=mimo n_antennas_2=2 n_antennas_5=2 dc2dc=0
