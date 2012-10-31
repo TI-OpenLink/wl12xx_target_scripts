0421b9e96c75039dcb8dde26100124a56eb52c4d
Yair Shapira <yair.shapira@ti.com>
2012-10-31 13:23:36 +0200

    testing-boot.sh: updated to start wl_logproxy automatically
    wl_logproxy is started without TCP port (writing logs to disk)
    
    Signed-off-by: Yair Shapira <yair.shapira@ti.com>

--- root/testing-boot.sh
+++ root/testing-boot.sh
@@ -2,3 +2,8 @@
 
 /system/bin/ifconfig eth0 20.1.1.20 netmask 255.255.255.0 up
 /system/xbin/busybox/telnetd -l sh
+
+mkdir -p /data/misc/wifi/fwlogs
+chmod 777 /data/misc/wifi/fwlogs
+
+/system/bin/wl_logproxy 0 /sys/devices/platform/omap/omap_hsmmc.4/mmc_host/mmc2/mmc2:0001/mmc2:0001:2/wl12xx/fwlog /data/misc/wifi/fwlogs/ 10000000 &
