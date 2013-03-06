ac45377e7d7a5977dc7d5747da878f17007fc0ac
Igal Chernobelsky <igalc@ti.com>
2013-03-06 14:22:54 +0200

    testing_set_wlcore: basic WL8_System_parameters.ini
    Support one basic INI file - WL8_System_parameters.ini
    Script modifies parameters for board types HDK/COM8 according to
    HT mode siso/mimo.
    
    Signed-off-by: Igal Chernobelsky <igalc@ti.com>

--- root/testing_set_wlcore.sh
+++ root/testing_set_wlcore.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # $1 = mimo/siso20/siso40
-# $2 = hw type - rdl1_rdl3/rdl2_rdl4
+# $2 = hw type - hdk/com8 
 
 echo $@
 
@@ -10,6 +10,7 @@ hw_type_fname=${root_path}/testing_wl_hw_type.txt
 
 wlconf_path=/usr/sbin/wlconf
 ini_files_path=${wlconf_path}/official_inis
+ini_file=WL8_System_parameters.ini
 
 wl18xx_conf_bin=/lib/firmware/ti-connectivity/wl18xx-conf.bin
 
@@ -22,6 +23,8 @@ hw_type=`cat $hw_type_fname 2> /dev/null`
 ht_mimo=0
 ht_siso20=2
 ht_siso40=1
+num_of_ant2_4=1
+low_band_component=2
 
 #
 # verify ht_mode, options are: siso20, siso40, mimo
@@ -32,43 +35,31 @@ elif  [ "$1" == "siso40" ] ; then
     ht_mode=${ht_siso40}
 elif  [ "$1" == "mimo" ] ; then
     ht_mode=${ht_mimo}
+    num_of_ant2_4=2
 elif  [ "$1" == "default" ] ; then
     ht_mode=${ht_mimo}
 else
-    echo "wlcore: not supported"
+    echo "wlcore: not supported ht mode"
     exit 1
 fi
 
 #
-# select chip type, options are: rdl2_rdl4 (mimo board), rdl1_rdl2 (hp siso board)
+# select chip type, options are: hdk or com8
 #
-if [ "$hw_type" == "rdl2_rdl4" ] ; then # mimo board
-    if [ "$ht_mode" == "$ht_mimo" ] ; then # mimo
-	ini_file=WL8_System_parameters_PG2_RDL_2_4_SP_MIMO.ini
-    elif [ "$ht_mode" == "$ht_siso40" ] || [ "$ht_mode" == "$ht_siso20" ] ; then # siso
-	ini_file=WL8_System_parameters_PG2_RDL_2_4_SP_SISO.ini
-    else
-	echo "wlcore: not supported"
-	exit 1
-    fi
-elif [ "$hw_type" == "rdl1_rdl3" ] ; then # hp siso board
-    if [ "$ht_mode" == "$ht_siso40" ] || [ "$ht_mode" == "$ht_siso20" ] ; then # siso
-	ini_file=WL8_System_parameters_PG2_RDL_1_3_HP_SISO.ini
-    elif [ "$ht_mode" == "$ht_mimo" ] ; then # mimo
-	echo "using HP siso board in default configuration"
-	ini_file=WL8_System_parameters_PG2_RDL_1_3_HP_SISO.ini
-    else
-	echo "wlcore: not supported"
-	exit 1
-    fi
+if [ "$hw_type" == "hdk" ] || [ "$hw_type" == "HDK" ] ; then
+	low_band_component=2
+elif [ "$hw_type" == "com8" ] || [ "$hw_type" == "COM8" ]  ; then
+	low_band_component=0xff
 else
-    echo "wlcore: not supported"
-    exit 1
+	echo "wlcore: not supported board type"
+	exit 1
 fi
 
 cd ${wlconf_path}
 ./wlconf -o ${wl18xx_conf_bin} -I ${ini_files_path}/${ini_file}
 ./wlconf -i ${wl18xx_conf_bin} -o ${wl18xx_conf_bin} --set wl18xx.ht.mode=${ht_mode}
+./wlconf -i ${wl18xx_conf_bin} -o ${wl18xx_conf_bin} --set wl18xx.phy.number_of_assembled_ant2_4=${num_of_ant2_4}
+./wlconf -i ${wl18xx_conf_bin} -o ${wl18xx_conf_bin} --set wl18xx.phy.low_band_component=${low_band_component}
 
 if [ "$3" != "" ]; then
     if [ "$3" == "no-a-band" ]; then
@@ -98,7 +89,7 @@ if [ "$4" != "" ]; then
     fi
 fi
 
-./wlconf -i ${wl18xx_conf_bin} -g | grep -i "board\|ant\|ht.mode\|recovery\|dc2dc\|sta_sleep_auth"
+./wlconf -i ${wl18xx_conf_bin} -g | grep -i "board\|ant\|ht.mode\|recovery\|dc2dc\|sta_sleep_auth\|low_band"
 
 echo "wlcore: configuration ok"
 exit 0
