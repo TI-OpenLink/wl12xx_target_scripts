9f8b89b1c6576fda56b009b57abcbe0059fdb50d
Yaniv Machani <yanivma@ti.com>
2013-08-26 14:31:56 +0300

    Fixed stop ap scripts - remove the correct interface

--- ap/ap_stop.sh
+++ ap/ap_stop.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-ehco "Terminating 1st AP"
+echo "Terminating 1st AP"
 
-ps | grep -v hostapd2 | grep h[o]stapd
+#ps | grep -v hostapd2 | grep h[o]stapd
 output=`ps | grep -v hostapd2 | grep h[o]stapd`
 set -- $output
 kill $1
 
-ps | grep -v udhcpd2 | grep u[d]hcpd
+#ps | grep -v udhcpd2 | grep u[d]hcpd
 output=`ps | grep -v udhcpd2 | grep u[d]hcpd`
 set -- $output
 kill $1
--- ap/ap_stop2.sh
+++ ap/ap_stop2.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-ehco "Terminating 2nd AP"
-ps | grep h[o]stapd2
+echo "Terminating 2nd AP"
+#ps | grep h[o]stapd2
 output=`ps |grep h[o]stapd2`
 set -- $output
 kill $1
 
 
 
-ps | grep u[d]hcpd2
+#ps | grep u[d]hcpd2
 output=`ps | grep u[d]hcpd2`
 set -- $output
+kill $1 
